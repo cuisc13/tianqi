9d4e3e231283cc9ebeddbb427c737f5711100d7d
崔士川 <cuisc13@gmail.com>
2017-04-08 14:11:08 +0800

    u 格式输出

--- tianqi.py
+++ tianqi.py
@@ -15,7 +15,6 @@ def get_ip():
 
     # net.cn 是国内站点速度快
     ip = html[html.find('<h2>')+4:html.find('</h2>')].split(', ')[0]
-    print(ip)
     print("当前IP：%s" % ip)
     return ip
 
@@ -38,19 +37,19 @@ def get_weather(city=None, ip=None):
     return jso
 
 def print_w(w):
-    fmt = u'''日期：    {0[time]}
-天气：    {0[condition]}
-风力：    {0[wind]}
-温度：    {0[temp]}
+    fmt = u'''{today}\t{0[time]}
+日期\t{0[date]}
+天气\t{0[condition]}
+风力\t{0[wind]}
+温度\t{0[temp]}
     '''
     content = w['data']['weather']['content']
     city = content['city']
     dayl = ['today','tomorrow','thirdday','fourthday', 'fifthday']
     daym = {'today':'今天','tomorrow':'明天','thirdday':'后天','fourthday':'第四天', 'fifthday':'第五天'}
-    print(u"查询的城市是%s" % city)
+    print(u"查询的城市: %s" % city)
     for day in dayl:
-        print(daym[day])
-        print(fmt.format(content[day],city=city))
+        print(fmt.format(content[day],today=daym[day].decode('utf8'), city=city))
     pass
 
 def main():
